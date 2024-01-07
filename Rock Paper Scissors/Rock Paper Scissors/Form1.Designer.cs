f7c54b9df1ef10fa2281482db6ec1f602e0d47ac
Kaan Kaya <kikitwin17@gmail.com>
2024-01-07 20:22:45 +0300

    Rock Paper Scissors Game C#
    A simple Rock Paper Scissors game made using C#

---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Hello_player = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Game_exit = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Game_Start = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Score_label = new System.Windows.Forms.Label();
+            this.Lap_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hello_player
+            // 
+            this.Hello_player.AutoSize = true;
+            this.Hello_player.BackColor = System.Drawing.Color.Transparent;
+            this.Hello_player.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Hello_player.ForeColor = System.Drawing.SystemColors.Control;
+            this.Hello_player.Location = new System.Drawing.Point(16, 9);
+            this.Hello_player.Name = "Hello_player";
+            this.Hello_player.Size = new System.Drawing.Size(302, 22);
+            this.Hello_player.TabIndex = 1;
+            this.Hello_player.Text = "Welcome Rock Paper Scissors game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(66, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Press \"Q\" to exit game";
+            // 
+            // Game_exit
+            // 
+            this.Game_exit.Tick += new System.EventHandler(this.Game_exit_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(201, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Developed By KAAN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(50, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Press \"Enter\" to exit game";
+            // 
+            // Game_Start
+            // 
+            this.Game_Start.Tick += new System.EventHandler(this.Game_Start_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.Control;
+            this.label7.Location = new System.Drawing.Point(66, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(152, 289);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 22);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(241, 289);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 22);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.SystemColors.Control;
+            this.label10.Location = new System.Drawing.Point(12, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 22);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Player";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.SystemColors.Control;
+            this.label11.Location = new System.Drawing.Point(272, 129);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 22);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "AI";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.ForeColor = System.Drawing.SystemColors.Control;
+            this.label12.Location = new System.Drawing.Point(18, 170);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(20, 22);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.ForeColor = System.Drawing.SystemColors.Control;
+            this.label13.Location = new System.Drawing.Point(254, 170);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(20, 22);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 236);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(127, 236);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(213, 236);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Score_label
+            // 
+            this.Score_label.AutoSize = true;
+            this.Score_label.BackColor = System.Drawing.Color.Transparent;
+            this.Score_label.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Score_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.Score_label.Location = new System.Drawing.Point(12, 364);
+            this.Score_label.Name = "Score_label";
+            this.Score_label.Size = new System.Drawing.Size(87, 22);
+            this.Score_label.TabIndex = 18;
+            this.Score_label.Text = "Score = 0";
+            // 
+            // Lap_label
+            // 
+            this.Lap_label.AutoSize = true;
+            this.Lap_label.BackColor = System.Drawing.Color.Transparent;
+            this.Lap_label.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lap_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.Lap_label.Location = new System.Drawing.Point(12, 342);
+            this.Lap_label.Name = "Lap_label";
+            this.Lap_label.Size = new System.Drawing.Size(71, 22);
+            this.Lap_label.TabIndex = 19;
+            this.Lap_label.Text = "Lap = 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(330, 395);
+            this.Controls.Add(this.Lap_label);
+            this.Controls.Add(this.Score_label);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hello_player);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(346, 434);
+            this.MinimumSize = new System.Drawing.Size(346, 434);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rock Paper Scissors";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GamePress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Hello_player;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Game_exit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer Game_Start;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label Score_label;
+        private System.Windows.Forms.Label Lap_label;
+    }
+}
+
---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label10.Hide();
+            label11.Hide();
+            label12.Hide();
+            label13.Hide();
+            Score_label.Hide();
+            Lap_label.Hide();
+        }
+
+        private void GameExit(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Game_exit_Tick(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Game_Start_Tick(object sender, EventArgs e)
+        {
+            label1.Hide();
+            label3.Hide();
+            Hello_player.Hide();
+            pictureBox1.Show();
+            pictureBox2.Show();
+            pictureBox3.Show();
+            label7.Show();
+            label8.Show();
+            label9.Show();
+            Lap_label.Show();
+            Score_label.Show() ;
+        }
+
+        private void GamePress(object sender, KeyEventArgs e)
+        {
+            string player;
+            switch (e.KeyCode)
+            {
+                case Keys.Q:
+                    Game_exit.Start();
+                    break;
+                case Keys.Enter:
+                    Game_Start.Start();
+                    break;
+                case Keys.NumPad1:
+                case Keys.D1:
+                    player = "Rock";
+                    player_choise = player;
+                    Player_ready();
+                    break;
+                case Keys.NumPad2:
+                case Keys.D2:
+                    player = "Paper";
+                    player_choise = player;
+                    Player_ready();
+                    break;
+                case Keys.NumPad3:
+                case Keys.D3:                  
+                    player = "Scissors";
+                    player_choise = player;
+                    Player_ready();
+                    break;               
+            }
+        }
+        string Ai_choise;
+        string player_choise;
+        public void AI()
+        {
+            string ai_choise;
+            Random rnd = new Random();
+            int Ai = rnd.Next(1,4);
+            if (Ai == 1)
+            {
+                ai_choise = "Rock";
+                Ai_choise = ai_choise;
+                label13.Text = Ai_choise;
+            }
+            if (Ai == 2)
+            {
+                ai_choise = "Paper";
+                Ai_choise = ai_choise;
+                label13.Text = Ai_choise;
+            }
+            if (Ai == 3)
+            {
+                ai_choise = "Scissors";
+                Ai_choise = ai_choise;
+                label13.Text = Ai_choise;                
+            }
+        }
+        int lap = 0;
+        int player_win = 0;
+        public void Winner()
+        {      
+            label12.Text = player_choise;
+            label13.Text = Ai_choise;
+            if (label12.Text =="Rock"&& label13.Text == "Rock")
+            {
+                MessageBox.Show("Tie!!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+            if (label12.Text == "Rock" && label13.Text == "Paper")
+            {
+                MessageBox.Show("AI Win!!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+            if (label12.Text == "Rock" && label13.Text == "Scissors")
+            {
+                MessageBox.Show("Player Win!!!");
+                lap += 1;
+                player_win += 1;
+                global_lap = lap;
+                global_score = player_win;
+                play_again();
+            }
+            if (label12.Text == "Paper" && label13.Text == "Paper")
+            {
+                MessageBox.Show("Tie!!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+            if (label12.Text == "Paper" && label13.Text == "Rock")
+            {
+                MessageBox.Show("Player Win!!!");
+                lap += 1;
+                player_win += 1;
+                global_lap = lap;
+                global_score = player_win;
+                play_again();
+            }
+            if (label12.Text == "Paper" && label13.Text == "Scissors")
+            {
+                MessageBox.Show("AI Win!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+            if (label12.Text == "Scissors" && label13.Text == "Scissors")
+            {
+                MessageBox.Show("Tie!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+            if (label12.Text == "Scissors" && label13.Text == "Paper")
+            {
+                MessageBox.Show("Player Win!!!");
+                lap += 1;
+                player_win += 1;
+                global_lap = lap;
+                global_score = player_win;
+                play_again();
+            }
+            if (label12.Text == "Scissors" && label13.Text == "Rock")
+            {
+                MessageBox.Show("AI Win!!!");
+                lap += 1;
+                global_lap = lap;
+                play_again();
+            }
+        }
+        int global_lap;
+        int global_score;
+        public void round_score()
+        {
+            Lap_label.Text = "Lap = " + global_lap.ToString();
+            Score_label.Text = "Score = " + global_score.ToString();
+        }
+        public void play_again()
+        {
+            label10.Hide();
+            label11.Hide();
+            label12.Hide();
+            label13.Hide();
+            pictureBox1.Show();
+            pictureBox2.Show();
+            pictureBox3.Show();
+            label7.Show();
+            label8.Show();
+            label9.Show();
+        }
+        public void Player_ready()
+        {
+            AI();          
+            label12.Text = player_choise;
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            pictureBox3.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            Game_Start.Stop();
+            label10.Show();
+            label11.Show();
+            label12.Show();
+            label13.Show();
+            Winner();
+            round_score();
+        }
+    }
+}
